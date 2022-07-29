6661f4565eed74c92ac99ea394bdef86ba82bbe3
WOOD22 <90883790+WOOD22@users.noreply.github.com>
2022-07-29 23:43:59 +0900

    2022.07.29

--- Assets/Script/Data/MapData.cs
+++ Assets/Script/Data/MapData.cs
@@ -51,16 +51,17 @@ public class Tile
 [Serializable]
 public class Tile_Resource
 {
-    public float tile_fertility;        //  비옥도, 농업 생산량+
+    public float tile_soil;             //  비옥도, 농업 생산량+
     public float tile_water;            //  강수량 & 저수지 등에 비축된 물이 포함된 수량, 인구 캡+, 농업 생산량+
     public float tile_tree;             //  수목량, 수량+, 인구 캡+
-    public float tile_coal;             //  석탄매장량
-    public float tile_copper;           //  구리매장량
-    public float tile_iron;             //  철매장량
-    public float tile_silver;           //  은매장량
-    public float tile_gold;             //  금매장량
-    public float tile_jewelry;          //  보석매장량
-    public float tile_elohium;          //  엘로히움매장량(엘로힘의 파편, 신적인 힘을 가진 금속, 가공자의 의지에 따라 가공품의 효과가 다르다.)
+    public float tile_stone;            //  석재량
+    public float tile_coal;             //  석탄 매장량
+    public float tile_copper;           //  구리 매장량
+    public float tile_iron;             //  철 매장량
+    public float tile_silver;           //  은 매장량
+    public float tile_gold;             //  금 매장량
+    public float tile_jewelry;          //  보석 매장량
+    public float tile_elohium;          //  엘로히움 매장량(엘로힘의 파편, 신적인 힘을 가진 금속, 가공자의 의지에 따라 가공품의 효과가 다르다.)
 }
 [Serializable]
 public class Tile_Property
@@ -68,7 +69,7 @@ public class Tile_Property
     public string name;                 //  타일 이름(인구 100 이상의 마을 부터 이름 생성)
     public string owner_code;           //  타일 소유세력 코드
     public string ruler_code;           //  타일 통치자 코드
-    public List<Pop> population         //  인구(계급, 문화별로 분류 )
+    public List<Pop> population         //  인구(계급, 문화별로 분류), 계급은 상류층, 중류층, 하류층으로 나뉜다.
         = new List<Pop>();
     public Development development      //  개발도
         = new Development();
@@ -86,13 +87,41 @@ public class Development
 {
     public float primary_industry_dev;  //  1차 산업
     public float primary_industry_cap;  //  상한
-    public float secondary_industry_dev;//  공업
-    public float secondary_industry_cap;
-    public float tertiary_industry_dev; //  상업
-    public float tertiary_industry_cap;
+    public float secondary_industry_dev;//  2차 산업
+    public float secondary_industry_cap;//  상한
+    public float tertiary_industry_dev; //  3차 산업
+    public float tertiary_industry_cap; //  상한
 }
 [Serializable]
-public class Industry
+public class Production
 {
-
+    //  1차 산업
+    public float grain;                 //  곡물
+    public float fish;                  //  생선
+    public float livestock;             //  가축
+    public float tree;                  //  목재
+    //  2차 산업
+    //  광업
+    public float stone;                 //  석재
+    public float coal;                  //  석탄
+    public float copper;                //  구리
+    public float iron;                  //  철
+    public float silver;                //  은
+    public float gold;                  //  금
+    public float jewelry;               //  보석
+    public float elohium;               //  엘로히움
+    //  공업
+    public float material;              //  자재
+    public float consumer;              //  소비재(가공된 식료품(빵, 소세지 등)을 뜻함), 계급 순으로 소비량 차이가 있으나 차이가 크지 않다.
+    public float durable;               //  내구재(의류, 개인 장구류, 마차 등), 계급 순으로 소비량 차이가 있고 차이가 크다.
+    public float luxury;                //  사치재(보석 장신구, 고급 의류 등), 하류층은 소비하지 않고 중류층과 상류층 간의 소비 차이가 크다.
+    public float supply;                //  보급품(병영에서 소비)
+    //  3차 산업
+    public float service;               //  서비스업
+    public float commerce;              //  상업
+    public float finance;               //  금융업
+    public float transport;             //  운송업
+    public float art;                   //  예술
+    public float science;               //  과학
+    public float humanity;              //  인문학
 }
